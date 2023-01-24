5ef0958070c0c63fbbfe1ca51ba29e81f6e7dbab
agata-zybula <agata.masiewicz@gmail.com>
2023-01-24 11:10:29 +0100

    save homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector("[data-start]");
+const buttonStop = document.querySelector("[data-stop]");
+const bodyColor = document.querySelector("body");
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+  if (
+    (timerId = setInterval(() => {
+      bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000))) {
+    buttonStart.disabled = true;
+  }
+});
+
+buttonStop.addEventListener('click', () => {
+  buttonStart.disabled = false;
+  clearInterval(timerId)
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,27 @@
+// Opisany w dokumentacji
+import flatpickr from 'flatpickr';
+// Dodatkowy import stylów
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const inputPickerDate = document.querySelector('input#datetime-picker');
+const buttonStart = document.querySelector('button[data-start]');
+
+let days = document.querySelector('span[data-days]');
+let hours = document.querySelector('span[data-hours]');
+let minutes = document.querySelector('span[data-minutes]');
+let seconds = document.querySelector('span[data-seconds]');
+
+
+const fp = flatpickr('input#datetime-picker', options);
+buttonStart.addEventListener('click', countdownOn);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
