538fdb480ff8d559645ad551c8ef499c653e78b8
agata-zybula <agata.masiewicz@gmail.com>
2023-01-24 19:26:36 +0100

    homework9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,24 +4,79 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const inputPickerDate = document.querySelector('input#datetime-picker');
-const buttonStart = document.querySelector('button[data-start]');
+const buttonStart = document.querySelector('[data-start]');
+buttonStart.disabled = true;
 
-let days = document.querySelector('span[data-days]');
-let hours = document.querySelector('span[data-hours]');
-let minutes = document.querySelector('span[data-minutes]');
-let seconds = document.querySelector('span[data-seconds]');
+const inputPickerDate = document.querySelector('#datetime-picker');
 
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
 
-const fp = flatpickr('input#datetime-picker', options);
-buttonStart.addEventListener('click', countdownOn);
+let userDate;
+const actualDate = new Date(); //zmienna z aktualna data
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
+let options = {
+  enableTime: true, //pokazuje czas do wyboru
+  time_24hr: true, //pokazuje czas w trybie 24h
+  defaultDate: new Date(), //domyslna, obecna data pokazywana w inpucie
+  minuteIncrement: 1, // wybor czasu z dokladnoscia do 1 minuty (default jest na 5)
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    //wybieramy date po zamknieciu kalendarza
+    if (selectedDates[0] < actualDate) { // jesli wybrana data przez uzytkownika jest z przeszlosci w porownaniu z aktualna data
+      buttonStart.disabled = true; //to przycisk jest nieaktywny
+      Notiflix.Notify.warning('Please choose a date in the future'); //i wyswietla sie ostrzezenie, aby wybrac date z przyszlosci
+    } else if (selectedDates[0] > actualDate) { //chyba, ze wybrana data jest z przyszlosci
+      buttonStart.disabled = false; //to przycisk staje sie aktywny
+      Notiflix.Notify.success('OK'); //i wyswietla sie komunikat o powodzeniu
+      userDate = selectedDates[0].getTime(); //i pobieramy wybrana przez uzytkownika date i lokujemy ja w zmiennej userDate
+    }
   },
+};
+
+const counterTime = () => { //odpalamy licznik czasu
+  buttonStart.disabled = true; //przycisk jest nieaktywny bo juz mamy liczony czas
+  const counter = setInterval(() => { //wykonuje funkcje co 1s
+    let gapTime = userDate - new Date().getTime(); //zmienna w ktorej jest roznica czasu miedzy wybranym czasem uzytkownika, a czasem aktualnym
+    let gapTimeMs = convertMs(gapTime); //zamiana tej roznicy na ms
+    if (gapTime <= 0) { //jesli roznica jest mniejsza lub rowna zero
+      clearInterval(counter); //to konczymy wykonywanie funkcji/odliczanie czasu
+    } else {
+      time(gapTimeMs); //w przeciwnym razie nadal liczymy czas
+    }
+  }, 1000);
+};
+
+const datePicker = flatpickr(inputPickerDate, options); //kalendarz
+buttonStart.addEventListener('click', counterTime); //na klik start rozpoczyna sie odliczanie czasu
+
+const time = gapTimeMs => {
+  days.textContent = gapTimeMs.days;
+  hours.textContent = gapTimeMs.hours;
+  minutes.textContent = gapTimeMs.minutes;
+  seconds.textContent = gapTimeMs.seconds;
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0'); //padStart - pierwszy parametr - dlugosc stringa - 2 miejsca, a drugi okresla jakim tekstem beda wypelniane brakujace znaki. jak nie podamy drugiego parametru to bedzie spacja.
 };
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from "notiflix";
+
+const form = document.querySelector('form');
+const inputFirstDelay = document.querySelector('input[name="delay"]');
+const inputSecondDelay = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+
+  
+function createPromise(position, delay) { //ktory promise (numer), opoznienie
+  return new Promise((resolve, reject) => { //zwroc promise resolve lub reject
+    setTimeout(() => { //opoznienie czasowe po jakim sie funkcja wykonuje
+      const shouldResolve = Math.random() > 0.3; //Math.random - wiekszy lub rowno 0 ale mniejszy niz 1
+      if (shouldResolve) { //jesli wylosuje liczbe <1 i >0.3 to jest resolve
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay }); //w przeciwnym wypadku reject
+      }
+    }, delay); //wartosc opoznienia z setTimeout
+  });
 }
+
+form.addEventListener("submit", (event) => { //po kiknieciu w button
+  event.preventDefault();
+  let delay = parseInt(inputFirstDelay.value); //zmienna let = parsuje tekst na liczbe calkowita
+  for (let i = 1; i <= amount.value; i++) { //ilosc promisow, dodawanie po 1
+    createPromise(i, delay) //odwolanie do funkcji createPromise, ktory promise i jego opoznienie
+      .then(({ position, delay }) => { //then obsluguje w praktyce pomyslne wykonanie promise
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => { //w catch wylapujemy bledy
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += parseInt(inputSecondDelay.value); //opoznienie calkowite = opoznienie+opoznienie kolejnej obietnicy wybranej przez uzytkownika
+  }
+});
