5a461d0f3c58ed5bdb18ac42f021560e3a7fcd5b
agata-zybula <110232577+agata-zybula@users.noreply.github.com>
2023-01-31 18:00:39 +0000

    Deploying to gh-pages from @ agata-zybula/goit-js-hw-09@b87cce97a8884b451ee534afaa8ddc8c5ca35ba1 🚀

--- 01-color-switcher.18660949.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");var o=null;t.addEventListener("click",(function(){(o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3))&&(t.disabled=!0)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.18660949.js.map
